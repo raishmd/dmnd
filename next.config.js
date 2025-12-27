41497552a87c6d08a9f863a0eb803d0152f52ebc
raishmd <rais7amidoo@gmail.com>
2025-12-27 13:47:14 +0100

    fix: Disable standalone output for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // Don't use standalone output for Vercel
+    output: undefined,
 };
 
 module.exports = withPWA(nextConfig);
